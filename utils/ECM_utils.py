[framework] bugfix: fixed issues with parameter scaling in circuit fitting tasks

utils/ECM_utils.py
@@ -165,7 +165,7 @@ class CircuitParams:
             if "alpha" in param.lower():
                 bds = (0, 1)
             else:
-                bds = (0, 10)
+                bds = (0, np.inf)
             self.bound.append(bds)
 
 class CircuitEvaluate:
@@ -182,19 +182,12 @@ class CircuitEvaluate:
         self.verbose = verbose
         self.scaling = scaling
         self.params_value = params_value
-
-        self.params_scaled = np.asarray(self.params_value)
-
-        for i in range(len(self.params_value)):
-            self.params_scaled[i] = self.params_value[i]*self.scaling[i]
-
+        self.params_scaled = self.params_value.flatten()*self.scaling.flatten()
         self.param_names = self.ecm.param_names
         self.params =dict(zip(self.param_names, self.params_scaled))
-
         self.Freqs = np.asarray(freqs, dtype=float)
         self.W = 2 * np.pi * self.Freqs
         self.tree = self.ecm.tree
-
         self.Z_ECM = self.eval_node(self.tree)
 
         if self.verbose:
@@ -273,7 +266,7 @@ class CircuitEvaluate:
         return z1 + z2
 
     def z_parallel(self, z1, z2):
-        return 1 / (1 / z1 + 1 / z2)
+        return 1 / ((1/z1) + (1/z2))
 
     def z_R(self, R, w):
         return R * np.ones_like(w, dtype=complex)

utils/data_types.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class SpectroscopyData:
-    def __init__(self, eis_data:np.ndarray, n_freqs:int=5, file_type:str='.csv', sheet_names:list=None):
+    def __init__(self, eis_data:np.ndarray, n_freqs:int=5, file_type:str='.csv', sheet_names:list=None, flip=False):
         '''
         :param eis_data : raw data output from the PHOBOS acquisition system
         :param n_freqs : number of frequencies
+        :param flip: flag to flip the matrices and arrays
         '''
 
         #check if the raw electrode data is a numpy array
@@ -32,11 +33,16 @@ class SpectroscopyData:
             self.Z_real = eis_data[:, 5]
             self.Z_imag = -eis_data[:, 6]
         elif file_type in [".xls", ".xlsx"]:
-            self.freq = eis_data[0,:, 0]
+            self.freq = eis_data[0,:,0]
+            if flip:
+                self.freq = self.freq[::-1]
             if self.freq.size != self.n_freqs:
                 raise TypeError(
                     f'[SpectroscopyData] Number of frequencies must be equal to {n_freqs}. Check your EIS file!')
             self.Z_real = eis_data[:,:, 2]
             self.Z_imag = -eis_data[:,:, 3]
+            if flip:
+                self.Z_real = np.flip(self.Z_real, axis=1)
+                self.Z_imag = np.flip(-self.Z_imag, axis=1)
             self.sheets = eis_data.shape[0]
 

utils/fitting_utils.py
@@ -2,8 +2,6 @@ from utils import data_types, ECM_utils
 import numpy as np
 from scipy.optimize import curve_fit, minimize, least_squares
 import time
-from functools import partial
-
 
 class Circuit_fitting:
     def __init__(self, data_medium:data_types.SpectroscopyData, freqs:np.ndarray, ecm: ECM_utils.CircuitParams):
@@ -26,7 +24,7 @@ class Circuit_fitting:
         #validate freqs
         if not isinstance(freqs, np.ndarray):
             raise TypeError(f'[EquivalentCircuit] "freqs" must be a Numpy Array! Curr. type = {type(freqs)}')
-        self.freqs = freqs
+        self.freqs = freqs.astype(float)
 
        #validate ECM data
         expected_ECMtypes = [ECM_utils.CircuitParams, list]
@@ -34,16 +32,7 @@ class Circuit_fitting:
             raise TypeError(f'[EquivalentCircuit] "ECM" must be {expected_ECMtypes}! Curr. type = {type(data_medium)}')
         self.ecm = ecm
 
-
-    def fit_circuit(self, initial_guess:np.ndarray, scaling_array:np.ndarray, method='BFGS', tol=1e-6, verbose=False):
-        '''
-        :param initial_guess: the initial guess for the fit to run the iterative algorithms
-        :param scaling_array: scale all the search parameters to avoid exploding gradients
-        :param method: which optimization algorithm will be used to fit the circuit data
-        :param tol: tolerance for the algorithms
-        :param verbose: flag to print the statistics in the terminal
-        :return: the parameters the best fit the expected equivalent circuit
-        '''
+        self.scaling = None
         self.fit_method_reponse = []
         self.opt_params = []
         self.opt_scaled_params = []
@@ -55,6 +44,16 @@ class Circuit_fitting:
         self.error_MAE = []
         self.inter_num = []
         self.fit_elapsed_time = []
+
+    def fit_circuit(self, initial_guess:np.ndarray, scaling_array:np.ndarray, method='BFGS', tol=1e-6, verbose=False):
+        '''
+        :param initial_guess: the initial guess for the fit to run the iterative algorithms
+        :param scaling_array: scale all the search parameters to avoid exploding gradients
+        :param method: which optimization algorithm will be used to fit the circuit data
+        :param tol: tolerance for the algorithms
+        :param verbose: flag to print the statistics in the terminal
+        :return: the parameters the best fit the expected equivalent circuit
+        '''
 
         #validate "method"
         valid_methods = ['BFGS', 'NLLS']
@@ -70,32 +69,33 @@ class Circuit_fitting:
             raise TypeError(f'[EquivalentCircuit] "initial_guess" must be a Numpy Array! Curr. type = {type(initial_guess)}')
 
         #validate "scaling_array"
-        if len(scaling_array) != len(initial_guess):
+        Z_meas_arr = np.atleast_2d(self.z_meas)
+        if len(scaling_array[0,:]) != len(initial_guess):
             raise ValueError(f'[EquivalentCircuit] Length of the scaling array do not match the length of the initial guess! {len(scaling_array)} != {len(initial_guess)}')
+        if scaling_array.shape[0] != Z_meas_arr.shape[0]:
+            raise ValueError(f'[EquivalentCircuit] The amount of scaling arrays do not match the amount of spectra! {scaling_array.shape[0]} != {Z_meas_arr.shape[0]}')
         self.scaling = scaling_array
 
         if not isinstance(scaling_array, np.ndarray):
             raise TypeError(f'[EquivalentCircuit] "scaling_array" must be a Numpy Array! Curr. type = {type(scaling_array)}')
 
         bounds = self.ecm.bound #optimization boundaries
-        Z_meas_arr = np.atleast_2d(self.z_meas)
-
         if self.fit_method == "BFGS":
             for i in range(Z_meas_arr.shape[0]):
                 z_raw = Z_meas_arr[i, :]
                 t_init = time.time()
-                min_obj = minimize(self.CUMSE, initial_guess, args=([z_raw, self.freqs, scaling_array]), bounds=bounds, method='L-BFGS-B')
+                min_obj = minimize(self.CUMSE, initial_guess, args=([z_raw.astype('complex'), self.freqs, scaling_array[i,:]]), bounds=bounds, method='L-BFGS-B')
                 t_elapsed = time.time() - t_init
-                opt_fit = ECM_utils.CircuitEvaluate(self.freqs, self.ecm, min_obj.x, self.scaling, verbose=False)
+                opt_fit = ECM_utils.CircuitEvaluate(self.freqs, self.ecm, min_obj.x, scaling_array[i,:], verbose=False)
                 Z_fit = opt_fit.Z_ECM
-                opt_params_scaled = min_obj.x*scaling_array #rescale the minimized parameters
+                opt_params_scaled = min_obj.x*scaling_array[i,:] #rescale the minimized parameters
                 nmse = self.NMSE(z_raw.astype("complex"), Z_fit.astype('complex')) #NMSE score for both complex parts
                 nrmse = self.NRMSE(z_raw.astype("complex"), Z_fit.astype('complex')) #nrmse score for both complex parts
                 chisqr = self.chi_square(z_raw.astype("complex"), Z_fit.astype('complex')) #chi-square score for both complex parts
                 mae = self.MAE(z_raw.astype("complex"), Z_fit.astype('complex')) #mae score for both complex parts
 
                 if verbose:
-                    print(f'[EquivalentCircuit] Gradient-based impedance fitting:')
+                    print(f'[EquivalentCircuit] Quasi-Newton-based impedance fitting:')
                     print(f'Test name: {self.data_medium.sheet_names[i]}')
                     print(f't = {t_elapsed} s')
                     print(f'NMSE = {nmse}')
@@ -132,12 +132,12 @@ class Circuit_fitting:
             for i in range(Z_meas_arr.shape[0]):
                 z_raw = Z_meas_arr[i, :]
                 t_init = time.time()
-                ls_obj = least_squares(self.CUMSE, x0=initial_guess, args=([[self.z_meas, self.freqs, scaling_array]]),
-                                       bounds=bounds, max_nfev=5000)  # Trust-Region-based NLLS
+                ls_obj = least_squares(self.CUMSE, x0=initial_guess, args=([[z_raw.astype('complex'), self.freqs, scaling_array[i,:]]]),
+                                       bounds=bounds, max_nfev=5000) #Trust-Region-based NLLS
                 t_elapsed = time.time() - t_init
-                opt_fit = ECM_utils.CircuitEvaluate(self.freqs, self.ecm, ls_obj.x, self.scaling, verbose=False)
+                opt_fit = ECM_utils.CircuitEvaluate(self.freqs, self.ecm, ls_obj.x, scaling_array[i,:], verbose=False)
                 Z_fit = opt_fit.Z_ECM
-                opt_params_scaled = ls_obj.x*scaling_array #rescale the minimized parameters
+                opt_params_scaled = ls_obj.x*scaling_array[i,:] #rescale the minimized parameters
                 nmse = self.NMSE(z_raw.astype("complex"), Z_fit.astype("complex")) #NMSE score for both complex parts
                 nrmse = self.NRMSE(z_raw.astype("complex"), Z_fit.astype("complex")) #nrmse score for both complex parts
                 chisqr = self.chi_square(z_raw.astype("complex"), Z_fit.astype("complex")) #chi-square score for both complex parts
@@ -181,7 +181,7 @@ class Circuit_fitting:
         :return: the mean squared error between the measured and fitted impedance values
         '''
 
-        ecm_hat = ECM_utils.CircuitEvaluate(self.freqs, self.ecm, theta, self.scaling, verbose=False)
+        ecm_hat = ECM_utils.CircuitEvaluate(args[1], self.ecm, theta, args[2], verbose=False)
         z_hat = ecm_hat.Z_ECM
         z_hat = z_hat.astype('complex')
         args[0] = args[0].astype('complex')

utils/linKK.py
@@ -24,10 +24,10 @@ class LinearKramersKronig:
         self.fit_type = 'complex'
         self.freqs = data_medium.freq
         self.z_meas_real = data_medium.Z_real
-        self.z_meas_imag = -data_medium.Z_imag
+        self.z_meas_imag = data_medium.Z_imag
 
         f_Hz = np.asarray(self.freqs, float).ravel()
-        z_meas = data_medium.Z_real + 1j * data_medium.Z_imag  # complex impedance
+        z_meas = data_medium.Z_real - 1j * data_medium.Z_imag  # complex impedance
         Z_meas_arr = np.atleast_2d(z_meas)
 
         for i in range(Z_meas_arr.shape[0]):